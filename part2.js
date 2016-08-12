f543ddabceedcbe0ccb886adee2800f45e7fc090
Jacob Burton <session1@START-Coding-MBP-03.local>
2016-08-12 15:27:20 -0700

    8.12.16 - Part 1

---
+++ part2.js
@@ -0,0 +1,11 @@
+var LeftSide = document.getElementById("leftSide");
+var face_number = 5;
+
+function generateFaces(){
+    while (face_number){
+
+        document.createElement();
+        face_number--;
+    }
+
+}
