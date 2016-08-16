e3dbc7ad9915289d7a50d81e2d5896bcbcae265e
Jacob Burton <session1@START-Coding-MBP-03.local>
2016-08-16 14:48:40 -0700

    8.16.16 - Part 3 and 4 finished correctly

--- part4.js
+++ part4.js
@@ -2,6 +2,7 @@ var LeftSide = document.getElementById("leftSide");
 var RightSide = document.getElementById("rightSide");
 var body = document.getElementsByTagName('body')[0];
 var face_number = 5;
+var face_size = 50;
 var img_src = "http://www.clker.com/cliparts/7/d/9/B/f/R/smiley-face-md.png";
 
 function generateFaces(){
@@ -9,7 +10,7 @@ function generateFaces(){
 
         var image = document.createElement("img");
         image.src = img_src;
-        image.style.height = "50px";
+        image.style.height = face_size + "px";
         image.style.top = Math.random() * 400 + "px";
         image.style.left = Math.random() * 400 + "px";
         LeftSide.appendChild(image);
@@ -20,5 +21,25 @@ function generateFaces(){
     RightSide.appendChild(LeftSideImages);
 
 
+    LeftSide.lastChild.onclick = function nextLevel(event){
+        event.stopPropagation();
+        while(LeftSide.firstChild){
+            LeftSide.removeChild(LeftSide.firstChild)
+        }
+        while(RightSide.firstChild){
+            RightSide.removeChild(RightSide.firstChild)
+        }
+        face_number += 5;
+        face_size -= 1;
+        generateFaces();
+    };
 }
 generateFaces();
+
+
+body.onclick = function gameOver() {
+    alert("You clicked the wrong one. \n\n Game Over!");
+
+    body.onclick = null;
+    LeftSide.lastChild.onclick = null;
+};
